493d6d828369e81c43f10002872a01ec4ca27f9c
Johnny Batcher <johnnyb@slickdemon.com>
2021-02-17 11:12:01 -0500

    pull should work

--- header.php
+++ header.php
@@ -63,7 +63,7 @@ record_template_include(array('dir' => dirname(__FILE__),'file' => basename(__FI
 			/**
 			 * sadasfd
 			 * asdasddsff
-			 * as666ertry again
+			 * as666ertry again one more thing
 			 */
 				sd_set_template_part(array( 'target_file' => 'header-layout.php' ));
 			?>		
