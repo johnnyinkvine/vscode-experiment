c4e4abd147a9611ff327686555cccd8b0bb08e76
Johnny Batcher <johnnyb@slickdemon.com>
2021-02-17 10:13:46 -0500

    header change

--- header.php
+++ header.php
@@ -63,7 +63,7 @@ record_template_include(array('dir' => dirname(__FILE__),'file' => basename(__FI
 			/**
 			 * sadasfd
 			 * asdasddsff
-			 * asdsdadfdsasda
+			 * as
 			 */
 				sd_set_template_part(array( 'target_file' => 'header-layout.php' ));
 			?>		
