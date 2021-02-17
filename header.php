c456dcc1e76a33b5dcdc526da87ef1f4fa0bf999
Johnny Batcher <johnnyb@slickdemon.com>
2021-02-17 18:29:38 -0500

    will it work

--- header.php
+++ header.php
@@ -63,7 +63,7 @@ record_template_include(array('dir' => dirname(__FILE__),'file' => basename(__FI
 			/**
 			 * sadasfd
 			 * asdasddsff
-			 * as666ertry again one mo I have this change here asasas
+			 * as666ertry again one mo I have this change here
 			 */
 				sd_set_template_part(array( 'target_file' => 'header-layout.php' ));
 			?>		
